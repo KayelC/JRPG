0d092dcbf911270a09dfc7ce66a9965aee2984b0
Kayel Calleja <kayelcalleja64@gmail.com>
2026-01-04 15:31:56 +0100

    Added New Skills
    Added New Darkness Skills.
    Added New Ice Skills.

--- JRPG/obj/Debug/net9.0/JRPG.AssemblyInfo.cs
+++ JRPG/obj/Debug/net9.0/JRPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa0a62c5781b398189c912ceec702424ccf749ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2906bb3172896b2444e82b8ab18cb91e643d6caa")]
 [assembly: System.Reflection.AssemblyProductAttribute("JRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

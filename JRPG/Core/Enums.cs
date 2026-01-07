2dc0b94448a08119376c44eacfd3204e83cda41d
Kayel Calleja <kayelcalleja64@gmail.com>
2026-01-07 16:35:08 +0100

    BattleManager Refactor
    The BattleManager has been reworked from the ground up, the old one can be found in the old branch.

--- JRPG/Core/Enums.cs
+++ JRPG/Core/Enums.cs
@@ -11,17 +11,17 @@ namespace JRPGPrototype.Core
     public enum Affinity
     {
         Normal,
-        Weak,   // Consumes minimal icons (Press Turn)
+        Weak,   // Consumes half icon
         Resist, // Normal damage
-        Null,   // Consumes extra icons (Penalty)
-        Repel,  // Consumes extra icons + Reflect
-        Absorb  // Consumes extra icons + Heal
+        Null,   // Consumes 2 icons
+        Repel,  // Consumes ALL icons + Reflect
+        Absorb  // Consumes ALL icons + Heal
     }
 
     public enum StatType
     {
         STR, MAG, AGI, END, LUK, // Common
-        INT, CHA                 // Player Exclusive
+        INT, CHA // Player Exclusive
     }
 
     public enum HitType
@@ -44,29 +44,26 @@ namespace JRPGPrototype.Core
         BlockEnd
     }
 
-    // Defines the "Job" or "Race" of the entity
     public enum ClassType
     {
-        Human,          // Basic, Item user, Gun user
-        PersonaUser,    // Can equip 1 Persona
-        WildCard,       // Can equip multiple Personas
-        Operator,       // Controls Demons, uses Commander Skills
-        Demon,          // Summoned entity
-        Avatar          // DDS Transformation (DLC)
+        Human,
+        PersonaUser,
+        WildCard,
+        Operator,
+        Demon,
+        Avatar
     }
 
-    // Defines who is pulling the strings (Crucial for Multiplayer)
     public enum ControllerType
     {
-        LocalPlayer,    // Input from keyboard/gamepad
-        AI,             // Controlled by script (Enemy or Auto-Battle Ally)
-        NetworkPlayer   // Controlled by remote client (Future proofing)
+        LocalPlayer,
+        AI,
+        NetworkPlayer
     }
 
-    // NEW: Defines tactical autonomy
     public enum ControlState
     {
-        DirectControl,  // Player chooses actions manually
-        ActFreely       // Entity acts based on AI logic
+        DirectControl,
+        ActFreely
     }
 }
